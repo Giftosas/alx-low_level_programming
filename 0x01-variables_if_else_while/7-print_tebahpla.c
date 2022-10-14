d1aea50844564dc7bf6c030e852d7eb76c2cbcf1
Giftosas <osariemenchantelle6@gmail.com>
2022-10-14 06:13:35 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'z'; low >= 'a'; low--)
+		putchar(low);
+	putchar('\n');
+
+	return (0);
+}
