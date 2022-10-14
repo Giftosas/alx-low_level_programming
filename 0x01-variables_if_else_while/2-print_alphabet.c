ed1bd1d6af911891bf8a6563f2e583cfd7da60c9
Giftosas <osariemenchantelle6@gmail.com>
2022-10-14 04:47:51 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		purchar('\n');
+
+	return (0);
+}
