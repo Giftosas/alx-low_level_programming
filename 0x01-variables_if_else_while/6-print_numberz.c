ccc6cdebea0babe49a04aac3fc42dbe8bd448f7e
Giftosas <osariemenchantelle6@gmail.com>
2022-10-14 05:46:45 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar (d;)
+	putchar('\n');
+
+	return (0);
+}
+
