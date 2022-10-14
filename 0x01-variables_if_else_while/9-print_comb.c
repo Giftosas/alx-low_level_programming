7bc054b7ea3d192884e1d5b8d871087e2c3a42b2
Giftosas <osariemenchantelle6@gmail.com>
2022-10-14 06:36:06 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+		if (d != '9')
+		{
+			putchar(',')
+				putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
