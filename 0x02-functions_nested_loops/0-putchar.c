3bd2b583b5d6fe26c6636a71cd012c54c2cd4df8
Giftosas <osariemenchantelle6@gmail.com>
2022-10-18 06:20:33 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,14 +5,15 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	_putchar('C');
+	_putchar('l');
+	_putchar('o');
+	_putchar('w');
+	_putchar('l');
+	_putchar('e');
+	_putchar('v');
+	_putchar('e');
+	_putchar('l');
 	_putchar('\n');
 
 	return (0);
