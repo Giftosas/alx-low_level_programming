eed3d74b3af03f07a94c77029df89d0f68ee12fd
Giftosas <osariemenchantelle6@gmail.com>
2022-10-27 13:30:10 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
+/**
+ * File: MAIN_H
+ * Auth: Amos Mwongela
+ * Desc: Header file containing prototypes for all functions
+ * used in the 0x05-pointers_arrays_strings directory.
+ */
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
