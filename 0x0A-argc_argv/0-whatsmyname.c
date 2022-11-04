317db2cfd4de482ba27e0d3f7a05e0e4bd297e04
Gift Osariemen Omokpamwan <osariemenchantelle6@gmail.com>
2022-11-04 16:42:32 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,1 @@
-#include "main.h"
-#include <stdio.h>
 
-/**
- * main - Print its name
- * argc: argument count
- * argv: argument vector
- *
- * Return: Always 0
- */
-int main(int argc, char *argv[])
-{
-	(void) argc;
-	printf("%s\n", argv[0]);
-
-	return (0);
-}
