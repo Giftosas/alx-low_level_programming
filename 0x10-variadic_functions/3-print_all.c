29d3d58bf9f3022363b9167be95c287fcb85bf29
Gift Osariemen Omokpamwan <osariemenchantelle6@gmail.com>
2022-11-17 15:29:02 +0100

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,19 +1,1 @@
-#include "variadic_functions.h"
-#include <stdlib.h>
-#include <stdio.h>
 
-/**
- * print_all - Prints anything
- * @format: The conversion specifier to prints
- *
- * Return: Nothing
- */
-void print_all(const char * const format, ...)
-{
-	va_list args;
-	f_dt form_types[] = {
-		{ "c", print_a_char },
-		{ "i", print_a_integer },
-		{ "f", print_a_float },
-		{ "s", print_a_char_ptr }
-	};
