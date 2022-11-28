236ba3a7a33d751aaf2401ab1b4df916843f87f0
Giftosas <osariemenchantelle6@gmail.com>
2022-11-28 14:03:43 -0800

    fix

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,8 +1,10 @@
-#ifndef LINK_LISTS_
-#define LINK_LISTS_
+#ifndef LINK_H
+#define LINK_H
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
 
 /**
  * struct listint_s - singly linked list
@@ -17,6 +19,7 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
